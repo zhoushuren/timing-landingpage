01a7a56f8b0afad170ea0dac5db010cc41ceb871
Davidv21r <166500770+Davidv21r@users.noreply.github.com>
2024-04-10 17:30:26 +0800

    Create Wallet.svelte

---
+++ src/lib/Wallet.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let connectedWallet = "";
+  export let address = "";
+
+  $: addressFirstPart = address.slice(0, -4);
+  $: addressLastPart = address.slice(-4);
+
+  function copyAddress() {
+    navigator.clipboard.writeText(address);
+    alert("Address copied to clipboard");
+  }
+</script>
+
+{#if connectedWallet}
+  <div class="flex flex-col gap-2">
+    <div
+      on:click={copyAddress}
+      class="flex flex-col gap-1 p-4 rounded-xl bg-stone-50 bg-opacity-0 hover:bg-opacity-10 transition-all duration-300 ease-in-out hover:cursor-pointer"
+    >
+      <span>{connectedWallet}</span>
+      <div class="flex text-xs text-stone-500 font-mono">
+        <span class="text-nowrap overflow-hidden text-ellipsis">{addressFirstPart}</span><span>{addressLastPart}</span>
+      </div>
+    </div>
+    <button
+      class="rounded-xl text-stone-500 px-4 py-3 bg-stone-50 bg-opacity-0 hover:bg-opacity-10 font-semibold transition-all duration-300 ease-in-out"
+      >Sign out</button
+    >
+  </div>
+{/if}
